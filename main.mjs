d38d451dddb05ec5c6b491f70c16d4f4639d7f67
kyurov <karateel@users.noreply.github.com>
2021-09-06 21:40:48 +0300

    added styles

--- main.mjs
+++ main.mjs
@@ -1,23 +1,26 @@
-import express from 'express';
-import mongoose from 'mongoose';
+import express from "express"
+import mongoose from "mongoose";
 import router from "./public/js/router.mjs";
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 import * as path from 'path';
 
+
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const PORT = 5000;
-
 const DB_URL = `mongodb+srv://user:user@cluster0.zimhd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
 const app = express();
 
 app.get('*', function (req, res) {
   res.sendFile(__dirname + '/public/index.html');
 });
 
-app.use(express.static(__dirname + '/artads/public'));
+// app.use(express.static(__dirname + '/artads/public'));
+app.use('/static', express.static(__dirname + '/public'));
 app.use('/', router)
 app.use(express.json())
 
